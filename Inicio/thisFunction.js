1c28813168a8b375079dbefbafd69e4e0e3a697f
unknown <eduardoeandroid@gmail.com>
2021-10-18 16:23:01 -0300

    funcoes get e set

--- Inicio/thisFunction.js
+++ Inicio/thisFunction.js
@@ -2,6 +2,17 @@ function criarPessoas(nome, sobrenome){
     return {
         nome,
         sobrenome,
+        //Getter
+        get nomeCompleto(){
+            return `${this.nome} ${this.sobrenome}`
+        },
+        //Setter
+        set nomeCompleto(novNome){
+            novNome = novNome.split(' ');
+            this.nome = novNome.shift();
+            this.sobrenome = novNome.join(' ')
+            console.log(novNome);
+        },
         fala(assunto){
             return `${this.nome} esta ${assunto}`;
         }
@@ -10,3 +21,6 @@ function criarPessoas(nome, sobrenome){
 
 const p1 = criarPessoas('Eduardo', 'Almeida');
 console.log(p1.fala("falando"));
+console.log(p1.nomeCompleto);
+p1.nomeCompleto = "Maria Joao Fernandes"
+console.log(p1.sobrenome);
