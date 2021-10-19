423cbb67bcd719c271212c5f328bf34c3e04e06f
unknown <eduardoeandroid@gmail.com>
2021-10-19 09:52:36 -0300

    Arrays

---
+++ Inicio/array.js
@@ -0,0 +1,24 @@
+const nomes = ['Eduardo', 'Maria', 'Joana']
+//const nomes = new Array('Eduardo', 'Maria', 'Joana')
+//nomes[2] = 'João';
+//console.log(nomes)
+//delete nomes[2]
+//console.log(nomes)
+//const novo = nomes;
+//novo.pop()
+//console.log(novo)
+//const novo = [...nomes];
+//const removido = novo.shift()
+//console.log(removido)
+nomes.push('João')
+nomes.unshift('Pedro')
+//const novo = nomes.slice(1,3);
+//novo.push('Marcos')
+
+const nome = "Eduardo Maria Pedro"
+const novo = nome.split(" ")
+console.log(novo)
+console.log(nomes)
+
+
+
