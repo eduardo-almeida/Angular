5d99623f892f6671eba46965f310e2ed6f27339e
unknown <eduardoeandroid@gmail.com>
2021-10-19 10:09:35 -0300

    Splice

--- Inicio/splice.js
+++ Inicio/splice.js
@@ -1,1 +1,5 @@
-const nomes = ['Eduardo', 'Maria', 'Joana']
+const nomes = ['Eduardo', 'Maria', 'Joana', 'Pedro', 'João']
+//const removidos = nomes.splice(-3, Number.MAX_VALUE);
+const removidos = nomes.splice(2, 2, 'Luiz');
+console.log(nomes)
+console.log(removidos)
