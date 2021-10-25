814a001c4b959fffb8ba251939dfed173476f33f
unknown <eduardoeandroid@gmail.com>
2021-10-25 09:53:29 -0300

    Iniciando curso de Angular

---
+++ Angular/app.ts
@@ -0,0 +1,11 @@
+let message: string = "Help me, Obi-wan Kenobi"
+console.log(message)
+
+let episode: number = 4
+console.log("this is episode " + episode)
+episode = episode +1
+console.log("O Novo é o " + episode)
+
+let favoriteDroid
+favoriteDroid = "R2-D2"
+console.log("My favodite droid is " + favoriteDroid)
---
+++ Angular/dist/app.js
@@ -0,0 +1,9 @@
+var message = "Help me, Obi-wan Kenobi";
+console.log(message);
+var episode = 4;
+console.log("this is episode " + episode);
+episode = episode + 1;
+console.log("O Novo é o " + episode);
+var favoriteDroid;
+favoriteDroid = "R2-D2";
+console.log("My favodite droid is " + favoriteDroid);
