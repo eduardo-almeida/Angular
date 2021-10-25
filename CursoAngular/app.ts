c83d0960e132fad8ded786670dbfa9e260851f23
unknown <eduardoeandroid@gmail.com>
2021-10-25 13:37:50 -0300

    Estudo jedi-academia

--- CursoAngular/app.ts
+++ CursoAngular/app.ts
@@ -9,3 +9,14 @@ console.log('O Novo é o ' + episode);
 let favoriteDroid;
 favoriteDroid = 'R2-D2';
 console.log('My favodite droid is ' + favoriteDroid);
+
+const isEnoughToBeatMF = function (parsecs: number): boolean {
+  return parsecs < 12;
+};
+
+const distance = 10;
+console.log(
+  `Is ${distance} parsecs enough to beat MIllenium Falcon? ${
+    isEnoughToBeatMF(distance) ? 'Yes' : 'No'
+  }`,
+);
