0fc129d595d545b8e76a2d5f166c9fda7b4c2979
unknown <eduardoeandroid@gmail.com>
2021-10-25 10:08:13 -0300

    Iniciando npm e typescript

--- Angular/dist/app.js
+++
@@ -1,9 +0,0 @@
-var message = "Help me, Obi-wan Kenobi";
-console.log(message);
-var episode = 4;
-console.log("this is episode " + episode);
-episode = episode + 1;
-console.log("O Novo é o " + episode);
-var favoriteDroid;
-favoriteDroid = "R2-D2";
-console.log("My favodite droid is " + favoriteDroid);
