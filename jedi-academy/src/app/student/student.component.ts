24a1b20f48d1a67263f82cd77b53fdb479e2bdca
unknown <eduardoeandroid@gmail.com>
2021-10-25 16:31:27 -0300

    Adcionando Eventos

--- jedi-academy/src/app/student/student.component.ts
+++ jedi-academy/src/app/student/student.component.ts
@@ -8,11 +8,15 @@ import {Student} from './students.model'
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
-  @Input() student: Student = {name : "", isJedi: true}
+  @Input() student: Student = {name : "Lalala", isJedi: true}
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  clicked(){
+    console.log(`Estudante: ${this.student.name} `)
   }
 
 }
