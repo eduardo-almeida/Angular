a0c230f28f74916f1b017c793cf61c39343ddcd5
unknown <eduardoeandroid@gmail.com>
2021-11-03 14:07:52 -0300

    Primeiro Modulo

--- meat-app-starter-master/src/app/app.routes.ts
+++ meat-app-starter-master/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from "@angular/router";
-import { AboutComponent } from "./about/about.component";
 import { HomeComponent } from "./home/home.component";
 import { JogadorComponent } from "./jogador/jogador.component";
 import { RestaurantsComponent } from "./restaurants/restaurants.component";
@@ -15,7 +14,7 @@ import { OrderSumaryComponent } from "./order-sumary/order-sumary.component";
 
 export const ROUTES: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
+  {path: 'about', loadChildren: './about/about.module#AboutModule'},
   {path: 'restaurants', component: RestaurantsComponent},
   {path: 'restaurants/:id', component: RestaurantDetailComponent,
     children: [
